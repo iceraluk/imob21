5d3fa00f24ddfb97b5f10b9a88b72963cce92990
Iulia Ionita <iulia.ionita@explovia.com>
2014-11-27 17:56:37 +0000

    Switch uploader to original setup

--- app/uploaders/offer_image_uploader.rb
+++ app/uploaders/offer_image_uploader.rb
@@ -1,51 +1,51 @@
 # encoding: utf-8
 
 class OfferImageUploader < CarrierWave::Uploader::Base
-  #include ::CarrierWave::Backgrounder::Delay
-  include CarrierWaveDirect::Uploader
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  include Sprockets::Helpers::RailsHelper
-  include Sprockets::Helpers::IsolatedHelper
-  process :convert => 'png'
 
-  include CarrierWave::MimeTypes
-  process :set_content_type
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
 
-  # Shop version
-  version :shop do
-    process :resize_to_fill => [640, 640]
-  end
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
 
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    if model.id
-      "uploads/offer_images/#{model.id}"
-    else
-      "uploads/offer_images/previews"
-    end
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def cache_dir
-    "#{Rails.root}/tmp/uploads"
-  end
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
 
-  def full_filename(for_file)
-    filename  = for_file.pathmap("%n")
-    extension = for_file.pathmap("%x")
-    [version_name, filename].compact.join('_') + extension
-  end
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
 
-  def default_url
-    "/assets/" + [version_name, "default_image.png"].compact.join('_')
-  end
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :scale => [50, 50]
+  # end
 
-  def will_include_content_type
-    true
-  end
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_white_list
+  #   %w(jpg jpeg gif png)
+  # end
 
-  default_content_type  'image/jpeg'
-
-  def full_filename(for_file)
-      [version_name, for_file.pathmap("%n")].compact.join('_') + for_file.pathmap("%x")
-  end
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
 
 end
