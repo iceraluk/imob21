2b4b4ab1351dd8c3b12389198e8d51460678b438
Iulia Ionita <iulia@ionita.me>
2014-12-07 01:50:18 +0000

    Finished index page listing design

---
+++ app/helpers/b2c_helper.rb
@@ -0,0 +1,51 @@
+module B2cHelper
+
+  def camere(nr_camere)
+    if nr_camere == 1
+      return "Camera"
+    else
+      return "Camere"
+    end
+  end
+
+  def pret(pret_vanzare, pret_inchiriere)
+    if pret_vanzare
+      return pret_vanzare
+    else
+      return pret_inchiriere
+    end
+  end
+
+  def pret_per(moneda, pret_per)
+    if moneda && (pret_per.nil? || pret_per.blank?)
+      return moneda
+    else
+      return moneda + " / " + pret_per
+    end
+  end
+
+  def operatiune(tip_operatiune)
+    if tip_operatiune == "Vanzare"
+      return 'vanzare'
+    elsif tip_operatiune == "Inchiriere"
+      return 'inchiriere'
+    end
+  end
+
+  def dormitoare(dormitoare)
+    if dormitoare == 1
+      return "Dormitor"
+    else
+      return "Dormitoare"
+    end
+  end
+
+  def bai(bai)
+    if bai == 1
+      return "Baie"
+    else
+      return "Bai"
+    end
+  end
+
+end
