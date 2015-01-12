fd4f7675f744dbb66d23b3b35f4132c43031505c
Iulia Ionita <iulia.ionita@explovia.com>
2015-01-12 12:44:39 +0000

    Removed a migration

--- db/migrate/20150112023201_remove_key_from_offer_image.rb
+++
@@ -1,9 +0,0 @@
-class RemoveKeyFromOfferImage < ActiveRecord::Migration
-  def up
-    remove_column :offer_images, :key
-  end
-
-  def down
-    add_column :offer_images, :key, :string
-  end
-end
