1224475a5fe108940c45402d6ce21b3974e496ac
Iulia Ionita <iulia@ionita.me>
2015-01-25 20:32:57 +0000

    Rezolvat titlu si email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -3,6 +3,13 @@ class MessageMailer < ActionMailer::Base
 
   def send_message(message)
     @message = message
-    mail(:to => message.agent.email, :subject => "[IMOB21] Mesaj nou")
+
+    email = message.offer.try(:admin).try(:email)
+
+    if email
+      mail(:to => email, :subject => "[IMOB21] Mesaj nou")
+    else
+      mail(:to => 'iulia@ionita.me', :subject => "[ERROR - IMOB21] Mesajul nu a fost trimis")
+    end
   end
 end
